53df148e8bf1160894c844d6794198580f21abe1
julesjacobs <julesjacobs@gmail.com>
2025-11-29 20:07:43 -0500

    Better printing

--- bin/ber_cli.ml
+++ bin/ber_cli.ml
@@ -61,38 +61,72 @@ let format_location (loc : Location.t) =
 let format_error (err : Parse.parse_error) =
   Format.asprintf "Error at %s: %s" (format_location err.loc) err.message
 
-let format_locations (locs : Location.t list) =
+type highlight =
+  { loc : Location.t
+  ; ch : char
+  ; label : string option
+  }
+
+let read_file_lines file =
+  try
+    let ic = open_in file in
+    let rec loop acc =
+      match input_line ic with
+      | line -> loop (line :: acc)
+      | exception End_of_file ->
+        close_in ic;
+        List.rev acc
+    in
+    Some (loop [])
+  with _ -> None
+
+let pack_highlights ranges =
+  let ranges = List.sort (fun (s1, _, _) (s2, _, _) -> compare s1 s2) ranges in
+  let rec place packed = function
+    | [] -> List.rev packed
+    | r :: rs ->
+      let rec try_place acc = function
+        | [] -> List.rev acc, None
+        | line :: rest ->
+          let last_end =
+            match List.rev line with
+            | [] -> -1
+            | (_, e, _) :: _ -> e
+          in
+          let s, _, _ = r in
+          if s > last_end then
+            (List.rev acc) @ ((line @ [ r ]) :: rest), Some ()
+          else
+            try_place (line :: acc) rest
+      in
+      (match packed with
+       | [] -> place [ [ r ] ] rs
+       | _ ->
+         let packed', placed = try_place [] packed in
+         let packed'' = match placed with None -> packed' @ [ [ r ] ] | Some () -> packed' in
+         place packed'' rs)
+  in
+  place [] ranges
+
+let format_highlights (locs : highlight list) =
   match locs with
   | [] -> ""
   | locs ->
-    let file = (List.hd locs).file in
-    let all_same_file = List.for_all (fun l -> l.file = file) locs in
+    let file = (List.hd locs).loc.file in
+    let all_same_file = List.for_all (fun l -> l.loc.file = file) locs in
     if not all_same_file then
-      String.concat "\n" (List.map format_location locs)
+      String.concat "\n" (List.map (fun h -> format_location h.loc) locs)
     else
-      let lines =
-        try
-          let ic = open_in file in
-          let rec loop acc =
-            match input_line ic with
-            | line -> loop (line :: acc)
-            | exception End_of_file ->
-              close_in ic;
-              List.rev acc
-          in
-          Some (loop [])
-        with _ -> None
-      in
-      match lines with
-      | None -> String.concat "\n" (List.map format_location locs)
+      match read_file_lines file with
+      | None -> String.concat "\n" (List.map (fun h -> format_location h.loc) locs)
       | Some file_lines ->
         let min_line =
-          List.fold_left (fun acc l -> min acc l.start.Lexing.pos_lnum) max_int locs
+          List.fold_left (fun acc h -> min acc h.loc.start.Lexing.pos_lnum) max_int locs
         in
         let max_line =
-          List.fold_left (fun acc l -> max acc l.stop.Lexing.pos_lnum) min_int locs
+          List.fold_left (fun acc h -> max acc h.loc.stop.Lexing.pos_lnum) min_int locs
         in
-        let buf = Buffer.create 128 in
+        let buf = Buffer.create 256 in
         for line_no = min_line to max_line do
           let line_content =
             if line_no - 1 < List.length file_lines then List.nth file_lines (line_no - 1) else ""
@@ -101,36 +135,57 @@ let format_locations (locs : Location.t list) =
           Buffer.add_string buf prefix;
           Buffer.add_string buf line_content;
           Buffer.add_char buf '\n';
-          let underline =
-            let len = String.length line_content in
-            let arr = Array.make len ' ' in
-            let apply_loc loc =
-              let start_line = loc.start.Lexing.pos_lnum in
-              let stop_line = loc.stop.Lexing.pos_lnum in
-              let start_col = loc.start.Lexing.pos_cnum - loc.start.Lexing.pos_bol in
-              let stop_col = loc.stop.Lexing.pos_cnum - loc.stop.Lexing.pos_bol in
-              if line_no < start_line || line_no > stop_line then ()
-              else
-                let s =
-                  if line_no = start_line then start_col else 0
-                in
-                let e =
-                  if line_no = stop_line then max s (stop_col - 1) else len - 1
-                in
-                let s = max 0 (min s (len - 1)) in
-                let e = max s (min e (len - 1)) in
-                for i = s to e do
-                  arr.(i) <- '^'
-                done
+          let line_ranges = List.filter (fun h -> line_no >= h.loc.start.Lexing.pos_lnum && line_no <= h.loc.stop.Lexing.pos_lnum) locs in
+          let underline_lines =
+            let ranges_for_line =
+              List.map
+                (fun h ->
+                   let start_col = if line_no = h.loc.start.Lexing.pos_lnum then h.loc.start.Lexing.pos_cnum - h.loc.start.Lexing.pos_bol else 0 in
+                   let stop_col =
+                     if line_no = h.loc.stop.Lexing.pos_lnum then max start_col (h.loc.stop.Lexing.pos_cnum - h.loc.stop.Lexing.pos_bol - 1)
+                     else (String.length line_content) - 1
+                   in
+                   let start_col = max 0 (min start_col (String.length line_content)) in
+                   let stop_col = max start_col (min stop_col (String.length line_content)) in
+                   (start_col, stop_col, h))
+                line_ranges
             in
-            List.iter apply_loc locs;
-            let underline_str = String.init len (fun i -> arr.(i)) in
-            underline_str
+            ranges_for_line |> pack_highlights
           in
-          if underline |> String.trim |> String.length > 0 then (
-            Buffer.add_string buf (String.make (String.length prefix) ' ');
-            Buffer.add_string buf underline;
-            Buffer.add_char buf '\n')
+          let prefix_spaces = String.make (String.length prefix) ' ' in
+          List.iter
+            (fun line ->
+               let arr = Array.make (String.length line_content) ' ' in
+              List.iter
+                (fun (s, e, h) ->
+                   for i = s to e do
+                     arr.(i) <- h.ch
+                   done)
+                 line;
+               if Array.exists (fun c -> c <> ' ') arr then (
+                 Buffer.add_string buf prefix_spaces;
+                 Buffer.add_string buf (String.init (Array.length arr) (fun i -> arr.(i)));
+                 Buffer.add_char buf '\n'))
+            underline_lines;
+          let messages =
+            line_ranges
+            |> List.filter_map (fun h ->
+                match h.label with
+                | None -> None
+                | Some msg ->
+                  let start_col = if line_no = h.loc.start.Lexing.pos_lnum then h.loc.start.Lexing.pos_cnum - h.loc.start.Lexing.pos_bol else 0 in
+                  Some (start_col, msg))
+          in
+          List.iter
+            (fun (col, msg) ->
+               Buffer.add_string buf prefix_spaces;
+               Buffer.add_string buf (String.make col ' ');
+               Buffer.add_string buf "|\n";
+               Buffer.add_string buf prefix_spaces;
+               Buffer.add_string buf (String.make col ' ');
+               Buffer.add_string buf msg;
+               Buffer.add_char buf '\n')
+            messages
         done;
         Buffer.contents buf
 
@@ -138,13 +193,13 @@ let format_type_error (err : Type_infer.type_error) =
   let kind_msg =
     match err.kind with
     | Type_infer.Type_mismatch (a, b) ->
-      Format.asprintf "type mismatch: %s vs %s" (Type_infer.string_of_ty a) (Type_infer.string_of_ty b)
+      Format.asprintf "Type mismatch: %s vs %s" (Type_infer.string_of_ty a) (Type_infer.string_of_ty b)
     | Type_infer.Occurs_check (tv, ty) ->
-      Format.asprintf "occurs check failed: %s occurs in %s" (Type_infer.string_of_ty (Type_solver.TVar tv)) (Type_infer.string_of_ty ty)
+      Format.asprintf "Occurs check failed: %s occurs in %s" (Type_infer.string_of_ty (Type_solver.TVar tv)) (Type_infer.string_of_ty ty)
     | Type_infer.Message msg -> msg
   in
-  let loc_block = format_locations [ err.loc ] in
-  Format.asprintf "Type error: %s\n%s" kind_msg loc_block
+  let loc_block = format_highlights [ { loc = err.loc; ch = '^'; label = None } ] in
+  Format.asprintf "%s\n%s" kind_msg loc_block
 
 let process_block_type filename (env : Type_infer.env) (block : File_format.block) =
   let has_content = List.exists (fun l -> String.trim l <> "") block.code_lines in
